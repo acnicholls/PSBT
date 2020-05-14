153887af92b248badab655f486fc2a9d453031d3
acnicholls <ac.nicholls@gmail.com>
2020-05-14 15:04:23 -0400

    working basic website

--- sourceBackupPortal/Models/SupportRequest.cs
+++ sourceBackupPortal/Models/SupportRequest.cs
@@ -1,9 +1,16 @@
-﻿namespace sourceBackup.Portal.Models
+﻿using System.ComponentModel;
+
+namespace sourceBackup.Portal.Models
 {
     public class SupportRequest
     {
+        [DisplayName("Your email")]
         public string Email { get; set; }
+
+        [DisplayName("CC me a copy")]
         public bool CarbonCopyUser { get; set; }
+
+        [DisplayName("email body (put your request details here)")]
         public string RequestBody { get; set; }
         public bool SendSuccess { get; set; } = false;
     }
